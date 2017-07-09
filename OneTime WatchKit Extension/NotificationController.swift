b19a5eb14567648a8d38187d3e9725e9c55c251e
Moaaz Sidat <moaaz.omer@gmail.com>
2017-07-09 11:43:38 -0400

    Initial Commit

---
+++ OneTime WatchKit Extension/NotificationController.swift
@@ -0,0 +1,43 @@
+//
+//  NotificationController.swift
+//  OneTime WatchKit Extension
+//
+//  Created by Moaaz Sidat on 2017-07-09.
+//  Copyright © 2017 MS. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    /*
+    override func didReceive(_ notification: UNNotification, withCompletion completionHandler: @escaping (WKUserNotificationInterfaceType) -> Swift.Void) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.custom)
+    }
+    */
+}
---
+++ OneTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneTime
+//
+//  Created by Moaaz Sidat on 2017-07-09.
+//  Copyright © 2017 MS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
